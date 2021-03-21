4d20c3cd30348019bdb8f7c34b82d283077a87bb
Humberto Delaserna <humberto.laserna@gmail.com>
2021-03-20 21:14:37 -0300

    #

---
+++ Novo_Caged_Estoque.R
@@ -0,0 +1,171 @@
+#        rm(list = ls())
+#     options(editor = 'notepad')
+library(tidyverse)
+library(lubridate)
+
+
+                        
+# CARREGAMENTO DOS MICRODADOS E PREPARAÇÃO DA BASE ----
+
+ # CNAE 2.3 ----
+ 
+  source('./CSV_Data/RAIS_CAGED/CNAE_2_3.R')
+ 
+cnae <- 
+   CNAE_Subclasses_2_0
+
+
+ # _____ SUBCLASSES alvo na seção B (Extrativa Mineral - Exceto petróleo & Gás) ----
+ subclasses_alvo_SECAO_B <-                    
+   c(#subclasse  denominação
+     "500301",	#Extração de carvão mineral
+     "500302",	#Beneficiamento de carvão mineral
+     "710301",	#Extração de minério de ferro
+     "710302",	#Pelotização, sinterização e outros beneficiamentos de minério de ferro
+     "721901",	#Extração de minério de alumínio
+     "721902",	#Beneficiamento de minério de alumínio
+     "722701",	#Extração de minério de estanho
+     "722702",	#Beneficiamento de minério de estanho
+     "723501",	#Extração de minério de manganês
+     "723502",	#Beneficiamento de minério de manganês
+     "724301",	#Extração de minério de metais preciosos
+     "724302",	#Beneficiamento de minério de metais preciosos
+     "725100",	#Extração de minerais radioativos
+     "729401",	#Extração de minérios de nióbio e titânio
+     "729402",	#Extração de minério de tungstênio
+     "729403",	#Extração de minério de níquel
+     "729404",	#Extração de minérios de cobre, chumbo, zinco e outros minerais metálicos não ferrosos não especificados anteriormente
+     "729405",	#Beneficiamento de minérios de cobre, chumbo, zinco e outros minerais metálicos não ferrosos não especificados anteriormente
+     "810001",	#Extração de ardósia e beneficiamento associado
+     "810002",	#Extração de granito e beneficiamento associado
+     "810003",	#Extração de mármore e beneficiamento associado
+     "810004",	#Extração de calcário e dolomita e beneficiamento associado
+     "810005",	#Extração de gesso e caulim
+     "810006",	#Extração de areia, cascalho ou pedregulho e beneficiamento associado
+     "810007",	#Extração de argila e beneficiamento associado
+     "810008",	#Extração de saibro e beneficiamento associado
+     "810009",	#Extração de basalto e beneficiamento associado
+     "810010",	#Beneficiamento de gesso e caulim associado à extração
+     "810099",	#Extração e britamento de pedras e outros materiais para construção e beneficiamento associado
+     "891600",	#Extração de minerais para fabricação de adubos, fertilizantes e outros produtos químicos
+     "892401",	#Extração de sal marinho
+     "892402",	#Extração de salgema
+     "892403",	#Refino e outros tratamentos do sal
+     "893200",	#Extração de gemas (pedras preciosas e semipreciosas)
+     "899101",	#Extração de grafita
+     "899102",	#Extração de quartzo
+     "899103",	#Extração de amianto
+     "899199",	#Extração de outros minerais não metálicos não especificados anteriormente
+     "990401",	#Atividades de apoio à extração de minério de ferro
+     "990402",	#Atividades de apoio à extração de minerais metálicos não ferrosos
+     "990403"	#Atividades de apoio à extração de minerais não metálicos
+   )
+ 
+ 
+ # _____ Lista classes Alvo na Seção C (Indústria de Transformação Relacionada não associada/consecutiva à mineração) ----
+ 
+ classes_alvo_SECAO_C <- 
+   c("20126",	        #          Fabricação de Intermediários para Fertilizantes 
+     "20134",		          #          Fabricação de Adubos e Fertilizantes 
+     "23206",		          #          Fabricação de Cimento 
+     "23303",		          #          Fabricação de Artefatos de Concreto, Cimento, Fibrocimento, Gesso e Materiais Semelhantes 
+     "23419",		          #          Fabricação de Produtos Cerâmicos Refratários 
+     "23427",		          #          Fabricação de Produtos Cerâmicos NãoRefratários para Uso Estrutural na Construção 
+     "23494",		          #          Fabricação de Produtos Cerâmicos NãoRefratários não Especificados Anteriormente 
+     "23915",		          #          Aparelhamento e Outros Trabalhos em Pedras 
+     "23923",		          #          Fabricação de Cal e Gesso 
+     "23991",		          #          Fabricação de Produtos de Minerais NãoMetálicos não Especificados Anteriormente 
+     "24113",		          #          Produção de FerroGusa 
+     "24121",		          #          Produção de Ferroligas 
+     "24211",		          #          Produção de SemiAcabados de Aço 
+     "24229",		          #          Produção de Laminados Planos de Aço 
+     "24237",		          #          Produção de Laminados Longos de Aço 
+     "24245",		          #          Produção de Relaminados, Trefilados e Perfilados de Aço 
+     "24318",		          #          Produção de Tubos de Aço com Costura 
+     "24393",		          #          Produção de Outros Tubos de Ferro e Aço 
+     "24415",		          #          Metalurgia do Alumínio e Suas Ligas 
+     "24423",		          #          Metalurgia dos Metais Preciosos 
+     "24431",		          #          Metalurgia do Cobre 
+     "24491",		          #          Metalurgia dos Metais NãoFerrosos e Suas Ligas não Especificados Anteriormente 
+     "24512",		          #          Fundição de Ferro e Aço 
+     "25314",		          #          Produção de forjados de aço e de metais nãoferrosos e suas ligas 
+     "24521",		          #          Fundição de Metais NãoFerrosos e Suas Ligas 
+     "32116")		          #          Lapidação de Gemas e Fabricação de Artefatos de Ourivesaria e Joalheria 
+ 
+ 
+ # __________ SUBCLASSES alvo na Seção C ----
+ 
+ subclasses_alvo_SECAO_C <- 
+   unique(cnae[cnae$classe %in% classes_alvo_SECAO_C, c("subclasse")])
+ 
+  
+ # _____ Estoque de Trabalhadores ----
+ 
+ arquivos <- c(
+   "EstoqueRefCaged2020_01_01.txt",
+   "EstoqueRefCaged2021_01_01.txt"
+   )
+ 
+ 
+ lista <- list()
+ for (i in 1:length(arquivos)) {
+   lista[[i]] <-
+     read.table(
+       paste('./CSV_Data/RAIS_CAGED/Novo_Caged/', arquivos[[i]], sep = ""),
+       header = TRUE,
+       sep = ";",
+       dec = ".",
+       #quote = "",
+       colClasses = c(
+         "character",
+         "character",
+         "numeric"
+         ),
+       stringsAsFactors = FALSE,
+       encoding = "UTF-8"
+       )
+   
+   if (i == 1) {
+      lista[[1]][c("data")] <- c('2020.01.01')
+      
+   } else {
+      
+      lista[[2]][c("data")] <- c('2021.01.01')
+   }
+   }
+ 
+ estoque_trabalhadores <- 
+   do.call("rbind", lista)
+ rm(lista)
+ 
+ 
+ select(estoque_trabalhadores, everything()) %>% group_by(data) %>% summarise(sum(estoqueref))
+ 
+ 
+ 
+ # delimitando pelas subclasses alvo das seções B e C ----
+ 
+ estoque_trabalhadores <- 
+    estoque_trabalhadores[estoque_trabalhadores$cnae20subclas %in% c(subclasses_alvo_SECAO_B, subclasses_alvo_SECAO_C),]
+ 
+ 
+ # Geocod e junção coluna de municípios ----
+ geocod <- 
+   read.table(file = "./CSV_Data/GeoCodigos_IBGE.csv", sep = ";", stringsAsFactors = FALSE, header = TRUE, 
+              colClasses = "character", encoding = 'iso-8859-1', quote = "") %>% as_tibble()
+ geocod$GEOCOD <- 
+   str_extract(geocod$GEOCOD, "......")
+ 
+ estoque_trabalhadores <- 
+   left_join(estoque_trabalhadores, geocod[, c("UF_sigla", "GEOCOD", "Município")], by = c("codmun" = "GEOCOD"))
+ 
+ 
+ # estoque de trabalhadore no início de cada ano 
+ 
+ summarise(group_by(estoque_trabalhadores, data), sum(estoqueref))
+ 
+ 
+ 
+ # exportando em RDS ---- 
+ 
+ saveRDS(movimentacao, file = "./CSV_Data/Novo_Caged_microdados_2020_secoes_CNAE23_B_C_ascii.RDATA", ascii = TRUE)
